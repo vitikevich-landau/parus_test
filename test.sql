0891dfc63beb68796f432f807989625fd2e92ba0
vitikevich-landau <vitikevich@mail.ru>
2020-10-10 01:12:02 +0700

    Текст на кириллице

--- test.sql
+++ test.sql
@@ -1,1 +1,1 @@
-select * from dba_tables where owner = 'PARUS'; /* Коммена ват на ки sd рила ва ице */
+select * from dba_tables where owner = 'PARUS'; /* Коммефыв фа ана ват на ки sd рила ва ице */
