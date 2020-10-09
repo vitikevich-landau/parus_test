eab6566bf87cfb5eec4250c5bc1638d24afb1eca
vitikevich-landau <vitikevich@mail.ru>
2020-10-09 16:27:12 +0700

    # Удалён последний JOIN

--- search_relations.sql
+++ search_relations.sql
@@ -10,8 +10,8 @@ FROM
        AND r.r_constraint_name = p.constraint_name
     INNER JOIN all_cons_columns   c
     ON c.constraint_name = r.constraint_name
-    INNER JOIN all_cons_columns   c1
-    ON c1.constraint_name = p.constraint_name
+--    INNER JOIN all_cons_columns   c1
+--    ON c1.constraint_name = p.constraint_name
 WHERE
     p.constraint_type IN (
         'P'
