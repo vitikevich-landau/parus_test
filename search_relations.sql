9943f5019a9867892c2220282b6190a819b6e67d
vitikevich-landau <vitikevich@mail.ru>
2020-10-09 16:19:00 +0700

    #init. Просмотр constraints таблиц

---
+++ search_relations.sql
@@ -0,0 +1,22 @@
+SELECT
+    p.table_name,
+    c1.column_name,
+    c.table_name,
+    c.column_name
+FROM
+    all_constraints    p
+    INNER JOIN all_constraints    r
+    ON r.constraint_type = 'R'
+       AND r.r_constraint_name = p.constraint_name
+    INNER JOIN all_cons_columns   c
+    ON c.constraint_name = r.constraint_name
+    INNER JOIN all_cons_columns   c1
+    ON c1.constraint_name = p.constraint_name
+WHERE
+    p.constraint_type IN (
+        'P'
+    )
+    AND p.table_name like 'CLNPERSONS' -- зависит от
+ORDER BY
+    r.table_name,
+    c.column_name
