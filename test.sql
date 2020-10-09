38ea5a5206438c1b8fabae938c57e886d0c650f9
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2020-10-10 01:59:46 +0700

    Работа с кириллическим текстом

--- test.sql
+++ test.sql
@@ -1,1 +1,1 @@
-select * from dba_tables where owner = 'PARUS'; /* Коммефыв фа ана ват н уау ауа ки sd фуацу рила ва ице */
+select * from dba_tables where owner = 'PARUS'; /* РљРёСЂРёР»Р»РёС‡РµСЃРєРёР№ С‚РµРєСЃС‚ */
