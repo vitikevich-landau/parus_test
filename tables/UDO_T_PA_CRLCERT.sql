1cac64a23f1c4e019a714cd7ff1718012b103b24
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2020-10-28 09:43:26 +0700

    # Создание таблиц CRLCERT

---
+++ tables/UDO_T_PA_CRLCERT.sql
@@ -0,0 +1,6 @@
+create table UDO_T_PA_CRLCERT
+(
+  serial_number   VARCHAR2(100),
+  revocation_date VARCHAR2(35),
+  insert_date     DATE
+);
---
+++ tables/UDO_T_PA_CRLCERT_TEMP.sql
@@ -0,0 +1,6 @@
+create table UDO_T_PA_CRLCERT_TEMP
+(
+  serial_number   VARCHAR2(100),
+  revocation_date VARCHAR2(35),
+  insert_date     DATE
+);
