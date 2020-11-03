fd605d72af7bc8a4bf4d5f53b4f7382bc15eb177
Viktor <vitikevich@gmail.com>
2020-11-03 20:30:11 +0700

    # Job, обновления списка отозванных/блокировка юзеров (rename)

--- jobs/UDO_J_PA_CHECK_CERTS.sql
+++ jobs/UDO_J_PA_CHECK_CERTS.sql
@@ -1,5 +1,5 @@
 begin
-  sys.dbms_scheduler.create_job(job_name            => 'PARUS.UDO_JOB_PA_CHECK_CERTS',
+  sys.dbms_scheduler.create_job(job_name            => 'PARUS.UDO_J_PA_CHECK_CERTS',
                                 job_type            => 'STORED_PROCEDURE',
                                 job_action          => 'UDO_P_PA_CHECK_CERTS',
                                 schedule_name       => 'PARUS.UDO_J_PA_CHECK_CERTS',
