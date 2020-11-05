9f747f649572c3bec331c161a2dc8a8f8994eb4b
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2020-11-05 12:27:17 +0700

    # Изменено имя планировщика

--- jobs/UDO_J_PA_CHECK_CERTS.sql
+++ jobs/UDO_J_PA_CHECK_CERTS.sql
@@ -2,7 +2,7 @@ begin
   sys.dbms_scheduler.create_job(job_name            => 'PARUS.UDO_J_PA_CHECK_CERTS',
                                 job_type            => 'STORED_PROCEDURE',
                                 job_action          => 'UDO_P_PA_CHECK_CERTS',
-                                schedule_name       => 'PARUS.UDO_J_PA_CHECK_CERTS',
+                                schedule_name       => 'PARUS.UDO_SCHED_PA_CHECK_CERTS',
                                 job_class           => 'DEFAULT_JOB_CLASS',
                                 enabled             => true,
                                 auto_drop           => false,
