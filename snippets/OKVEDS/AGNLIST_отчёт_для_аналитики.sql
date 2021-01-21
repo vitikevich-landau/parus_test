f174e99e1165ca36897f05505b664079c0d03793
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-01-21 16:58:39 +0700

    # Добавление общего отчёта

--- snippets/OKVEDS/AGNLIST_отчёт_для_аналитики.sql
+++ snippets/OKVEDS/AGNLIST_отчёт_для_аналитики.sql
@@ -59,4 +59,78 @@ from
             and A.AGNIDNUMB is not null
     ) A
 where
-    A.INN_CNT > 1;
+    A.INN_CNT > 1
+;
+
+
+/*
+    Общий отчёт по вышеперечисленным
+    not in для инвертирования
+*/
+select
+    *
+from
+    AGNLIST A
+where
+    A.AGNTYPE = 0
+    and A.RN not in (
+        select
+            A.RN
+        from
+            (
+                ( select
+                    *
+                from
+                    AGNLIST A
+                where
+                    A.AGNTYPE = 0
+                    and A.AGNIDNUMB is null
+                )
+                union all
+                ( select
+                    *
+                from
+                    AGNLIST A
+                where
+                    A.AGNTYPE = 0
+                    and A.REASON_CODE is null
+                    or LENGTH(A.REASON_CODE) < 3
+                )
+                union all
+                ( select
+                    *
+                from
+                    AGNLIST A
+                where
+                    A.AGNTYPE = 0
+                    and A.FULLNAME is null
+                    or LENGTH(A.FULLNAME) < LENGTH(A.AGNNAME)
+                )
+                union all
+                ( select
+                    *
+                from
+                    AGNLIST A
+                where
+                    A.RN in (
+                        select
+                            A.RN
+                        from
+                            (
+                                select
+                                    count(*) over(
+                                        partition by A.AGNIDNUMB
+                                    ) INN_CNT,
+                                    A.*
+                                from
+                                    AGNLIST A
+                                where
+                                    A.AGNTYPE = 0
+                                    and A.AGNIDNUMB is not null
+                            ) A
+                        where
+                            A.INN_CNT > 1
+                    )
+                )
+            ) A
+    );
