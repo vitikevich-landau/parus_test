c711bbb4e02deb2cd9d108d7eac1d051259e1e89
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-01-22 10:39:07 +0700

    # Добавлены выборки по нужным полям

--- snippets/OKVEDS/AGNLIST_отчёт_для_аналитики.sql
+++ snippets/OKVEDS/AGNLIST_отчёт_для_аналитики.sql
@@ -6,7 +6,13 @@
     Пустые ИНН
 */
 select
-    *
+    A.RN,
+    A.AGNABBR,
+    A.AGNNAME,
+    A.FULLNAME,
+    A.AGNTYPE,
+    A.AGNIDNUMB,
+    A.REASON_CODE
 from
     AGNLIST A
 where
@@ -18,7 +24,13 @@ where
     Пустые/Невалидные КПП
 */
 select
-    *
+    A.RN,
+    A.AGNABBR,
+    A.AGNNAME,
+    A.FULLNAME,
+    A.AGNTYPE,
+    A.AGNIDNUMB,
+    A.REASON_CODE
 from
     AGNLIST A
 where
@@ -31,7 +43,13 @@ where
     Пустые/Невалидные полные наименования
 */
 select
-    *
+    A.RN,
+    A.AGNABBR,
+    A.AGNNAME,
+    A.FULLNAME,
+    A.AGNTYPE,
+    A.AGNIDNUMB,
+    A.REASON_CODE
 from
     AGNLIST A
 where
@@ -44,7 +62,14 @@ where
     Записи с дублирующими ИНН
 */
 select
-    *
+    A.INN_CNT,
+    A.RN,
+    A.AGNABBR,
+    A.AGNNAME,
+    A.FULLNAME,
+    A.AGNTYPE,
+    A.AGNIDNUMB,
+    A.REASON_CODE
 from
     (
         select
@@ -68,69 +93,109 @@ where
     not in для инвертирования
 */
 select
-    *
+    count(*) over(
+        partition by A.AGNIDNUMB
+    ) INN_MATCH,
+    count(*) over(
+        partition by A.AGNIDNUMB, A.REASON_CODE, A.AGNNAME, A.FULLNAME
+    ) MATCH,
+    A.*
 from
-    AGNLIST A
-where
-    A.AGNTYPE = 0
-    and A.RN in (
+    (
         select
-            A.RN
+            A.AGNABBR,
+            A.AGNNAME,
+            A.FULLNAME,
+            A.AGNTYPE,
+            A.AGNIDNUMB,
+            A.REASON_CODE
         from
-            (
-                ( select
-                    *
+            AGNLIST A
+        where
+            A.AGNTYPE = 0
+            and A.RN in (
+                select
+                    A.RN
                 from
-                    AGNLIST A
-                where
-                    A.AGNTYPE = 0
-                    and A.AGNIDNUMB is null
-                )
-                union all
-                ( select
-                    *
-                from
-                    AGNLIST A
-                where
-                    A.AGNTYPE = 0
-                    and A.REASON_CODE is null
-                    or LENGTH(A.REASON_CODE) < 3
-                )
-                union all
-                ( select
-                    *
-                from
-                    AGNLIST A
-                where
-                    A.AGNTYPE = 0
-                    and A.FULLNAME is null
-                    or LENGTH(A.FULLNAME) < LENGTH(A.AGNNAME)
-                )
-                union all
-                ( select
-                    *
-                from
-                    AGNLIST A
-                where
-                    A.RN in (
-                        select
-                            A.RN
+                    (
+                        ( select
+                            A.RN,
+                            A.AGNABBR,
+                            A.AGNNAME,
+                            A.FULLNAME,
+                            A.AGNTYPE,
+                            A.AGNIDNUMB,
+                            A.REASON_CODE
                         from
-                            (
-                                select
-                                    count(*) over(
-                                        partition by A.AGNIDNUMB
-                                    ) INN_CNT,
-                                    A.*
-                                from
-                                    AGNLIST A
-                                where
-                                    A.AGNTYPE = 0
-                                    and A.AGNIDNUMB is not null
-                            ) A
+                            AGNLIST A
                         where
-                            A.INN_CNT > 1
-                    )
-                )
-            ) A
-    );
+                            A.AGNTYPE = 0
+                            and A.AGNIDNUMB is null
+                        )
+                        union all
+                        ( select
+                            A.RN,
+                            A.AGNABBR,
+                            A.AGNNAME,
+                            A.FULLNAME,
+                            A.AGNTYPE,
+                            A.AGNIDNUMB,
+                            A.REASON_CODE
+                        from
+                            AGNLIST A
+                        where
+                            A.AGNTYPE = 0
+                            and A.REASON_CODE is null
+                            or LENGTH(A.REASON_CODE) < 3
+                        )
+                        union all
+                        ( select
+                            A.RN,
+                            A.AGNABBR,
+                            A.AGNNAME,
+                            A.FULLNAME,
+                            A.AGNTYPE,
+                            A.AGNIDNUMB,
+                            A.REASON_CODE
+                        from
+                            AGNLIST A
+                        where
+                            A.AGNTYPE = 0
+                            and A.FULLNAME is null
+                            or LENGTH(A.FULLNAME) < LENGTH(A.AGNNAME)
+                        )
+                        union all
+                        ( select
+                            A.RN,
+                            A.AGNABBR,
+                            A.AGNNAME,
+                            A.FULLNAME,
+                            A.AGNTYPE,
+                            A.AGNIDNUMB,
+                            A.REASON_CODE
+                        from
+                            AGNLIST A
+                        where
+                            A.RN in (
+                                select
+                                    A.RN
+                                from
+                                    (
+                                        select
+                                            count(*) over(
+                                                partition by A.AGNIDNUMB
+                                            ) INN_CNT,
+                                            A.*
+                                        from
+                                            AGNLIST A
+                                        where
+                                            A.AGNTYPE = 0
+                                            and A.AGNIDNUMB is not null
+                                    ) A
+                                where
+                                    A.INN_CNT > 1
+                            )
+                        )
+                    ) A
+            )
+    ) A;
