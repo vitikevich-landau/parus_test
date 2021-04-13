57badb55828ce7372e4b1e22042bad0431428235
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-04-13 14:10:00 +0700

    # Add LATECOMERS crate table scripts

---
+++ solutions/LATECOMERS/UDO_T_PA_LATECOMERS.sql
@@ -0,0 +1,7 @@
+create table UDO_T_PA_LATECOMERS (
+    FULLNAME   varchar2(100),
+    D          varchar2(2),
+    M          varchar2(2),
+    Y          varchar2(4),
+    VAL        varchar2(30)
+);
---
+++ solutions/LATECOMERS/UDO_T_PA_PARUS_EMPLOYEES.sql
@@ -0,0 +1,3 @@
+create table UDO_T_PA_PARUS_EMPLOYEES (
+    FULLNAME   varchar2(100)
+);
